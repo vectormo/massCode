3910dbcc547639b779889df6f5ef45accf09e0bc
vectormo <zy8121@gmail.com>
2022-10-20 16:33:33 +0800

    Update and rename index.html to index.js

---
+++ wxError/index.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+var useragent = navigator.userAgent;
+if (useragent.match(/MicroMessenger/i) != 'MicroMessenger')
+{
+window.location.href = "wxError.html";
+}
+</script>
