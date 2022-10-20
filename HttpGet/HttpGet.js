9f9beb3ed2414ceef3394c57a5e3c790b95d0169
vectormo <zy8121@gmail.com>
2022-10-20 16:37:11 +0800

    Create HttpGet.js

---
+++ HttpGet/HttpGet.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+  var httpRequest = new XMLHttpRequest();//第一步：建立所需的对象
+  httpRequest.open('GET', '', true);//第二步：声明请求方式，写入链接
+	httpRequest.send();//第三步：发送请求  将请求参数写在URL中
+</script>
